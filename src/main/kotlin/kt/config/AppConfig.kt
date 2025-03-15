c99f6fd36f746d16d62b1e95cbb37acb79738756
Owen Goh <owengoh2001@gmail.com>
2025-03-15 23:45:21 +0800

    fix/remove prints for keys

--- src/main/kotlin/kt/config/AppConfig.kt
+++ src/main/kotlin/kt/config/AppConfig.kt
@@ -1,19 +1,11 @@
 package kt.config
 
-import jakarta.annotation.PostConstruct
 import jakarta.enterprise.context.ApplicationScoped
 import org.eclipse.microprofile.config.inject.ConfigProperty
-import org.slf4j.LoggerFactory
 
 @ApplicationScoped
 class AppConfig {
-    private val logger = LoggerFactory.getLogger(this::class.java)
 
     @ConfigProperty(name = "quarkus_langchain4j_openai_api_key", defaultValue = "dummy")
     lateinit var openAiApiKey: String
-
-    @PostConstruct
-    fun onInit() {
-        logger.info("OpenAI API Key: $openAiApiKey")
-    }
 }
